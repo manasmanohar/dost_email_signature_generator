288cdb027f9f73aa6392e77a8979147a2360e11a
manasmanohar <21006907+manasmanohar@users.noreply.github.com>
2023-11-02 12:30:22 +0530

    resolveed length issue, added new  html render,copy singature btn

--- components/CodePreview.js
+++ components/CodePreview.js
@@ -4,9 +4,6 @@ export default function codepreview({ data, generateCode, }) {
   
   const generatedCode = generateCode(data);
 
-  const handleCopyCode = () => {
-    alert('Copied to Clipboard');
-  };
 
 return (
     <div className="text-black">
@@ -14,7 +11,7 @@ return (
 
       <div className='border border-gray-400 text-black rounded-lg my-5  p-3 flex flex-col'>
         <textarea className="w-full  border-2 h-32 p-4 rounded-lg bg-[#F8F9FA]" defaultValue={generatedCode} />
-        <button id="copy-button"className=" mt-12 p-2 bg-blue-500 text-white rounded" onClick={handleCopyCode}>Copy Code</button>
+        <button id="copy-button"className=" mt-4 p-2 bg-blue-500 text-white rounded" >Copy Code</button>
       </div>
      
     </div>
